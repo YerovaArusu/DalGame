669ee907ed5141eead380b615c8d9951775cf7b0
Marcel Micheu <marcel.micheu@edu.htl-klu.at>
2024-01-18 12:11:37 +0100

    [Feature] Further implementation of the inventory and collectibles

--- Assets/resources/Background/Menu/Slot_UI.cs
+++ Assets/resources/Background/Menu/Slot_UI.cs
@@ -1,72 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[System.Serializable]
-public class Inventory
+
+public class Slot_UI : MonoBehaviour
 {
-   [System.Serializable]
-   public class Slot
-   {
-   public Collectible_Type type;
-   public int count;
-   public int maxAllowed;
-
-   public Slot()
-   {
-      type = Collectible_Type.NONE;
-      count = 0;
-      maxAllowed = 99;
-   }
-
-   public bool CanAddItem()
-   {
-      if (count < maxAllowed)
-      {
-         return true;
-      }
-
-      return false;
-   }
-
-   public void AddItem(Collectible_Type type)
-   {
-      this.type = type;
-      count++;
-   }
+    
 }
-
-   public List<Slot> slots = new List<Slot>();
-
-   public Inventory(int numSlots)
-   {
-      for (int i = 0; i < numSlots; i++)
-      {
-         Slot slot = new Slot();
-         slots.Add(slot);
-      }
-     
-      
-   }
-
-   public void Add(Collectible_Type typeToAdd)
-   {
-      foreach (Slot slot in slots)
-      {
-         if (slot.type == typeToAdd && slot.CanAddItem())
-         {
-            slot.AddItem(typeToAdd);
-            return;
-         }
-      }
-      foreach (Slot slot in slots)
-      {
-         if (slot.type == Collectible_Type.NONE)
-         {
-            slot.AddItem(typeToAdd);
-            return;
-         }
-      }
-   }
-
-}
- 
