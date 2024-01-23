76fa79b32f3bcbdf9c0ddd4a7245c49b90d51028
Marcel Micheu <marcel.micheu@edu.htl-klu.at>
2024-01-23 07:49:11 +0100

    [Fix] Fixed the worldgen and increased performance

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -15,11 +16,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] public GameObject defaultWorldTile;
     [SerializeField] public LayerMask mapLayer;
     
+    [Header("Coins")]
+    [SerializeField] public GameObject coinPrefab;
+    [SerializeField] public int maxCoins = 10;
+    
     [Header("Enemy")]
     [SerializeField] public GameObject enemyPrefab;
     [SerializeField] public int maxEnemies = 10;
     
     private GameObject[] enemiesOnMap;
+    private GameObject[] coinsOnMap;
     
     
     void Start()
@@ -29,11 +35,13 @@ public class GameManager : MonoBehaviour
         
         
         
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        generateCoins();
         enemiesOnMap = GameObject.FindGameObjectsWithTag("Enemy");
         
         if (enemiesOnMap.Length < maxEnemies)
@@ -42,7 +50,7 @@ public class GameManager : MonoBehaviour
             float spawnY = MathHelper.getRandomFloat(-1 * mapHeight / 2, mapHeight / 2);
 
             GameObject enemy = enemyPrefab;
-            Instantiate(enemy, new Vector2(spawnX, spawnY), Quaternion.Euler(0, 0, 0));
+            Instantiate(enemy, new Vector3(spawnX, spawnY,-8f), Quaternion.Euler(0, 0, 0));
         }
     }
 
@@ -76,49 +84,75 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void generateWorldMap()
+
+   public void generateWorldMap()
     {
         GameObject mapParent = Instantiate( new GameObject("WorldMap"), new Vector3(0,0,0), Quaternion.Euler(0,0,0)) as GameObject;
 
-        for (int xPos = (-1* mapWidth/2); xPos <= mapWidth/2; xPos++)
+        for (int xPos = (-1* mapWidth/2); xPos <= mapWidth/2; xPos += (int) (1.5 * defaultWorldTile.transform.localScale.x))
         {
-            for (int yPos = (-1* mapHeight/2); yPos <= mapHeight/2; yPos++)
+            for (int yPos = (-1* mapHeight/2); yPos <= mapHeight/2; yPos += (int) (1.5 * defaultWorldTile.transform.localScale.y))
             {
-                float f = generateNoise(xPos, yPos, 8f);
+                float f = generateNoise(xPos, yPos, 16f);
 
                 GameObject defaultWorldObj = defaultWorldTile;
                 defaultWorldObj.transform.position = new Vector3(xPos, yPos, 0f);
                 Instantiate(defaultWorldObj, mapParent.transform);
                 
-                
-                if (f > 0.7f && Random.Range(0, 100) > 95)
+                if (f > 0.6f && Random.Range(0, 100) > 90)
                 {
                     GameObject worldObj = getRandomGameObject(worldPrefabs);
                     worldObj.transform.position = new Vector3(xPos, yPos, -0.01f);
                     worldObj.transform.Rotate(Vector3.forward, 90 * Random.Range(0,3), Space.World);
-
                     
-                    
-
-                    Instantiate(worldObj, mapParent.transform);
-                    
-                    //Does not work... YET
-                    Collider2D[] hitColliders = Physics2D.OverlapBoxAll(worldObj.transform.position, transform.localScale / 2, 0f, mapLayer);
+                    Collider2D[] hitColliders = Physics2D.OverlapBoxAll(worldObj.transform.position, transform.localScale, 0f, mapLayer);
                     if (hitColliders.Length > 0)
                     {
-                        for (int i = 0; i < hitColliders.Length; i++)
+                        List<GameObject> collidersWithSameName = new List<GameObject>();
+                        
+                        foreach (Collider2D hitCollider in hitColliders)
                         {
-                            Destroy(hitColliders[i].GameObject());
+                            if (hitCollider.GameObject().name.Contains(worldObj.name))
+                            {
+                                collidersWithSameName.Add(hitCollider.GameObject());
+                            }
+                        }
+                        
+                        foreach (GameObject o in collidersWithSameName)
+                        {
+                            Destroy(o);
                         }
                     }
+                    
+                    Instantiate(worldObj, mapParent.transform);
                 }
-                
-
-                
-                
             }
         }
     }
+
+    private void generateCoins()
+    {
+        coinsOnMap = GameObject.FindGameObjectsWithTag("Coin");
+        List<GameObject> coins = new List<GameObject>(coinsOnMap);
+     
+        if (coins.Count() < maxCoins)
+        {
+        for (int xPos = (-1 * mapWidth / 2); xPos <= mapWidth / 2; xPos++)
+        {
+            for (int yPos = (-1 * mapHeight / 2); yPos <= mapHeight / 2; yPos++)
+            {
+                float f = generateNoise(xPos, yPos, 5f);
+                if (f >= 0.5 && Random.Range(0, 100) > 95 && coins.Count() < maxCoins)
+                {
+                    GameObject coin = coinPrefab;
+                    coin.transform.position = new Vector3(xPos, yPos, -9f);
+                    coins.Add(coin);
+                    Instantiate(coin);
+                }
+            }
+        }
+    }
+}
 
     private float generateNoise(int x, int y, float detailScale)
     {
